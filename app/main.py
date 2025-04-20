82d43b00570217307e37e777de55c2d2c9ffad35
Rodo Mendoza Lucar <rodomendozalucar@192.168.1.19>
2025-04-19 21:00:33 -0400

    connec to firestore db

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.clients.firestore import get_firestore_client
 
 app = FastAPI()
 
@@ -16,3 +17,18 @@ app.add_middleware(
 @app.get("/")
 def hello_world():
     return { "message": "Hello world! AGAIN" }
+
+
+@app.get("/notes")
+async def get_notes():
+    db = get_firestore_client()
+    collection_ref = db.collection("test-notes-mac")
+
+    notes = []
+
+    docs = collection_ref.stream()
+    for doc in docs:
+        note_data = doc.to_dict()
+        notes.append(note_data)
+    
+    return { "notes": notes }
